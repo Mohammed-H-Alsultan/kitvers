663a97f84776f1fbe677f586be3cf222c5c64e64
Mohammed-H-ALsultan <alsultan.mohammedh@gmail.com>
2026-02-19 23:19:06 +0300

    adding header and description

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="bg-black/40 p-5 flex items-center border-b border-white/15">
+      <h1 className="font-ibm mr-3 font-bold text-2xl">KitVers</h1>
+      <p className="font-ibm text-[#474747] ">v0.0.1</p>
+      <div className="ml-auto font-mono">
+        <div className="inline-grid *:[grid-area:1/1]">
+          <div className="status status-info rounded-full animate-ping"></div>
+          <div className="status status-info rounded-full mr-2"></div>
+        </div>
+        Woek in progress
+      </div>
+    </header>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import { MdCreateNewFolder } from "react-icons/md";
+import { FaFolderOpen } from "react-icons/fa";
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col justify-center items-center h-[70vh]">
+        <p className="font-mono text-[#474747] text-start mr-79 mb-5">// Project workspace</p>
+      <h1 className="font-jetbrains font-extrabold text-9xl text-white">
+        Kit
+        <span className="font-jetbrains font-light text-9xl text-[#474747]">
+          Vers
+        </span>
+      </h1>
+
+      <div className="mt-10 w-110 mr-5 border-l border-white/30 p-3">
+        <p className="font-mono font-light ">
+          Stop wiring up the same stack twice.
+          <span className="font-extrabold text-[#888]"> KitVers</span> scaffolds
+          your React project pre-loaded with the libraries you actually use
+          <span className="font-extrabold text-[#888]">
+            {" "}
+            icons, charts, routing, DaisyUI, and more
+          </span>{" "}
+          — fully configured and ready to ship. Pick your kit. Build what
+          matters.
+        </p>
+      </div>
+
+      <div className="mt-10">
+        <button
+          className="btn bg-white text-black mr-9 
+              shadow-[0px_0px_0px_0px_#ffffff] 
+              hover:shadow-[0px_0px_20px_1px_#ffffff4d]
+              transition-shadow duration-300 ease-in-out"
+        >
+          <MdCreateNewFolder size={20} className="mr-1" />
+          New project +
+        </button>
+        <button
+          className="btn bg-[#474747] text-white
+              shadow-[0px_0px_0px_0px_#ffffff] 
+              hover:shadow-[0px_0px_20px_1px_#ffffff4d]
+              transition-shadow duration-300 ease-in-out pr-6
+            "
+        >
+          <FaFolderOpen size={20} className="mr-2" />
+          Old projects
+        </button>
+      </div>
+    </div>
+  );
+}
